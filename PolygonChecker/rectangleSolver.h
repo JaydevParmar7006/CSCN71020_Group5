3ab50ffd10442e45ce7b33106577aad3caaa844c
Shreemn Yadav <Syadav9215@conestogac.on.ca>
2024-11-28 23:40:10 -0500

    Commit #7 Rectangle Solver .h

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -4,3 +4,4 @@ bool isRectangle(double points[4][2]);
 double calculatePerimeter(double points[4][2]);
 double calculateArea(double points[4][2]);
 void sortRectanglePoints(double points[4][2]);
+double calculateDistance(double x1, double y1, double x2, double y2);
