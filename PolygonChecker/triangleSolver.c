d7e395c252eaf85d871568ba541c81628df961b8
Jaydevsinh Himmatsinh Parmar <Jparmar1742@conestogac.on.ca>
2024-11-28 22:33:32 -0500

    Commit #1: changed function's datatype in header files and created a result varibale holding the answer

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,10 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <math.h>
+#include <stdlib.h>
 #include "triangleSolver.h"
 
-char* analyzeTriangle(int side1, int side2, int side3) {
-	char* result = "";
+#define PI 3.14159265358979323846
+#define EPSILON 1e-6 
+
+char* analyzeTriangle(double side1, double side2, double side3) {
+	char* result = (char*)malloc(150 * sizeof(char));
+	if (result == NULL) {
+		printf("Memory allocation is failed.\n");
+		return NULL;
+	}
+
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
 		result = "Not a triangle";
 	}
